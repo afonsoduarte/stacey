7d798a62bd92e340a95c37be3ebe1d050ff3250f
Afonso Martins <afonsomartins@gmail.com>
2013-04-06 13:12:08 +0100

    cleanup

--- templates/js/script.js
+++ templates/js/script.js
@@ -5,14 +5,4 @@ $(function() {
   //Init Gallery
   if($('.project .media').length > 0) $('.project .media').gallery();
 
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-  function getRandomImage() {
-    return images[ getRandomInt(0, images.length - 1) ];
-  }
-
-  if($('.random-image').length > 0) $('.random-image').append('<img src="'+ getRandomImage() +'" />');
-
 });
